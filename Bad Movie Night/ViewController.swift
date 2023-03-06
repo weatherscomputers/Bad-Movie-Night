6ca8046a7e50b65df2abb4ce853d988dcf123721
weatherscomputers <markweathers84@gmail.com>
2023-03-05 22:49:51 -0500

    Initial Commit

---
+++ Bad Movie Night/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bad Movie Night
+//
+//  Created by Mark Weathers on 3/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
